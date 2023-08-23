1e4fd2fdac5f4d3cf317256f2d36d3b42537bab3
Suhas Aggarwal <suhasagg@gmail.com>
2023-08-23 22:23:17 +0530

    Aggregation jobs for different job types like rewards module jobs, liquid stake jobs, redeem stake jobs, distribute liquidity jobs, distribute redeemed tokens job
    Aggregation jobs for different job types like rewards module job, liquid stake job, redeem stake job, distribute liquidiity job, distribute redeem token job
    Aggregation jobs for job data integration with Notification system, Machine Learning module. 
    For example - Job crash report in case of central liquid stake job error, ibc-transfer error, redeem tokens at maximum redemption rate job error, Anomaly Detection Machine Learning jobs, Forecasting Machine Learning jobs, generate tally algorithm reports.
    Data format is also useful for Advance/Multi-metric/Custom Dimension, Custom Aggregation/Filters based reports and graphs.

--- photosynthesisv12/photosynthesis-main/sap-with-full-liquid-stake-redemption-workflow/ingestionscripts/distributeredeemtokenaggregator.py
+++ photosynthesisv12/photosynthesis-main/sap-with-full-liquid-stake-redemption-workflow/ingestionscripts/distributeredeemtokenaggregator.py
@@ -3,38 +3,50 @@ from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk, scan
 import json
 
+# Establish a connection to the Elasticsearch instance
 es = Elasticsearch('http://localhost:9200')
 
 def format_value(value):
+    """
+    Convert a string value to int, float, boolean, or retain as string.
+    """
+    # Try converting to integer
     try:
         return int(value)
     except ValueError:
         pass
 
+    # Try converting to float
     try:
         return float(value)
     except ValueError:
         pass
 
+    # Convert 'true' and 'false' strings to boolean
     if value.lower() in ['true', 'false']:
         return value.lower() == 'true'
 
+    # Return original string if no conversions apply
     return value
 
 def process_log(document):
+    """
+    Extract the '_source' content from the Elasticsearch document.
+    """
     try:
         if '_source' in document:
             log = document['_source']
-            #print(log)
             return log
         else:
-            #print(f"Document does not contain '_source' field: {document}")
             return None
     except Exception as e:
-        #print(f"Failed to process document: {document}. Error: {e}")
         return None
 
 def get_latest_timestamp(index_name):
+    """
+    Fetch the latest timestamp ('ts') from a given Elasticsearch index.
+    """
+    # Query the latest document based on timestamp
     body = {
         "size": 1,
         "sort": [
@@ -53,9 +65,12 @@ def get_latest_timestamp(index_name):
     else:
         print("No 'ts' field found in the latest document or no documents present in the index.")
         return None
-
       
 def fetch_documents_after_timestamp(index_name, timestamp):
+    """
+    Retrieve all documents from a specified index after a given timestamp.
+    Uses scrolling for efficient retrieval of large amounts of data.
+    """
     body = {
         "query": {
             "range": {
@@ -78,41 +93,38 @@ def fetch_documents_after_timestamp(index_name, timestamp):
 
     return documents
 
-
-
-
 def transform_document(log):
     """
-    Modify this function to apply transformations on the document fetched from source index.
-    For now, this will return the log as is.
+    Apply transformations to the document.
+    In this placeholder function, a 'processed' field is added.
     """
-    # Example transformation: add a new field
     log['processed'] = True
     return log
-    
-
-from datetime import datetime, timedelta
-from elasticsearch.helpers import scan, bulk
 
 def process_log(document):
-    # Placeholder function for processing the log.
-    # Modify it to suit your actual log processing needs.
+    """
+    Placeholder function for processing the log.
+    """
     return document['_source']
 
 def round_to_nearest_three_minutes(ts_str):
+    """
+    Round a timestamp to its nearest 3-minute mark.
+    """
     dt = datetime.strptime(ts_str, "%Y-%m-%dT%H:%M")
     rounded_minute = (dt.minute // 3) * 3
     return dt.replace(minute=rounded_minute, second=0)
 
 def process_and_aggregate_documents_from_index(source_index, target_index, bulk_size=20000):
+    """
+    Fetch logs, group them in 3-minute intervals, and insert into a target index.
+    """
     bulk_ops = []
     grouped_docs = {}
 
-    # Using scan to get all the documents from the source index
     for document in scan(es, index=source_index):
         processed_log = process_log(document)
         if processed_log and 'message' in processed_log:
-            # Convert and round the timestamp to the nearest 3-minute mark
             rounded_time = round_to_nearest_three_minutes(processed_log['timestamp'][:16])
             ts_str = rounded_time.strftime("%Y-%m-%dT%H:%M")
 
@@ -122,31 +134,29 @@ def process_and_aggregate_documents_from_index(source_index, target_index, bulk_
                     'message_1': processed_log['message']
                 }
             else:
-                count = 2  # starting from the second message
+                count = 2  # start from the second message
                 while f"message_{count}" in grouped_docs[ts_str]:
                     count += 1
                 grouped_docs[ts_str][f"message_{count}"] = processed_log['message']
 
     for ts, doc in grouped_docs.items():
-        print(f"Preparing bulk operation for timestamp {ts}")  # Debugging line
-        print(doc)
         bulk_ops.append({
             "_index": target_index,
             "_source": doc
         })
 
         if len(bulk_ops) == bulk_size:
-            print(f"Bulk operation size reached: {bulk_size}. Committing bulk operation.")  # Debugging line
             bulk(es, bulk_ops)
             bulk_ops = []
 
     if bulk_ops:
-        print(f"Committing remaining operations. Count: {len(bulk_ops)}")  # Debugging line
         bulk(es, bulk_ops)
 
-
 def create_or_update_index_with_mappings(index_name):
-    # Define the mapping
+    """
+    Create or update an Elasticsearch index with specified mappings.
+    If the index exists, it gets overwritten (use with caution).
+    """
     mapping = {
         "mappings": {
             "properties": {
@@ -162,8 +172,7 @@ def create_or_update_index_with_mappings(index_name):
                             "ignore_above": 2147483647
                         }
                     }
-                },
-                # Add other fields as needed
+                }
             }
         }
     }
@@ -171,12 +180,13 @@ def create_or_update_index_with_mappings(index_name):
     if not es.indices.exists(index=index_name):
         es.indices.create(index=index_name, body=mapping)
     else:
-        # Be cautious with this, as reindexing will be needed if you're updating an existing index's mapping
-        # For this example, I'm assuming the index is either new or can be safely overwritten
         es.indices.delete(index=index_name)
         es.indices.create(index=index_name, body=mapping)
 
 def main():
+    """
+    Main execution function.
+    """
     source_index_name = "distributeredeemtokensdata"
     target_index_name = "distributeredeemtokensdataaggregated"
 
@@ -189,7 +199,5 @@ def main():
     else:
         process_and_aggregate_documents_from_index(source_index=source_index_name, target_index=target_index_name)
 
-
 if __name__ == "__main__":
     main()
-
