d467b1fe1d9ad15421b259ab78adf2dc2d4754b1
Suhas Aggarwal <suhasagg@gmail.com>
2024-09-12 18:09:36 +0530

    Added lib.rs

---
+++ prototypesindev/johnwick/src/lib.rs
@@ -0,0 +1,40 @@
+pub mod contract;
+
+pub use crate::contract::{
+    instantiate, execute, query, HandleMsg, QueryMsg, InitMsg, StakeResponse,
+};
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use cosmwasm_std::{Addr, coins, Uint128, Empty}; 
+    use cw_multi_test::{App, Contract, ContractWrapper, Executor};  
+
+    fn mock_app() -> App {
+        App::default()
+    }
+
+    fn contract_john_wick() -> Box<dyn Contract<Empty>> {
+        let contract = ContractWrapper::new(execute, instantiate, query);
+        Box::new(contract)
+    }
+
+    #[test]
+    fn test_instantiate() {
+        let mut app = mock_app();
+        let code_id = app.store_code(contract_john_wick());
+
+        let contract_addr = app.instantiate_contract(
+            code_id,
+            Addr::unchecked("creator"),
+            &InitMsg {},
+            &[],
+            "John_Wick",
+            None,
+        ).unwrap();
+
+        assert_eq!(contract_addr, Addr::unchecked("contract0"));
+    }
+
+   
+}
